c911ad595ddaed55ad14f149479e9bf594820468
Thales Alves <thalesalves758@gmail.com>
2023-01-21 16:42:16 -0300

    feat: create GET /tweets route

--- src/main/java/com/tweteroo/api/controller/TweetController.java
+++ src/main/java/com/tweteroo/api/controller/TweetController.java
@@ -1,14 +1,21 @@
 package com.tweteroo.api.controller;
 
+import java.util.List;
+
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.tweteroo.api.dto.ResponseTweetDTO;
 import com.tweteroo.api.dto.TweetDTO;
 import com.tweteroo.api.exception.UserNotExists;
 import com.tweteroo.api.model.Tweet;
@@ -35,5 +42,16 @@ public class TweetController {
     } catch (UserNotExists e) {
       return new ResponseEntity<>(e.getMessage(), HttpStatus.UNAUTHORIZED);
     }
+  }
+
+  @GetMapping()
+  public List<ResponseTweetDTO> getTweets(
+      @PageableDefault(sort = "id", direction = Sort.Direction.DESC, page = 0, size = 5) Pageable page) {
+    List<Tweet> tweets = tweetService.getTweets(page);
+
+    List<ResponseTweetDTO> response = tweets.stream().map((tweet) -> modelMapper.map(tweet, ResponseTweetDTO.class))
+        .toList();
+
+    return response;
   }
 }
