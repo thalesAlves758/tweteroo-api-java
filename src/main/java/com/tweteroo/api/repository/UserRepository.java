954dceab05fd52a42ea6004f6e9ee94129d478ea
Thales Alves <thalesalves758@gmail.com>
2023-01-21 12:39:34 -0300

    fix: change User model to TweterooUser model

--- src/main/java/com/tweteroo/api/repository/UserRepository.java
+++ src/main/java/com/tweteroo/api/repository/UserRepository.java
@@ -1,9 +1,11 @@
 package com.tweteroo.api.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.tweteroo.api.model.User;
+import com.tweteroo.api.model.TweterooUser;
 
-public interface UserRepository extends JpaRepository<User, Integer> {
-
+public interface UserRepository extends JpaRepository<TweterooUser, Integer> {
+  public Optional<TweterooUser> findByUsername(String username);
 }
