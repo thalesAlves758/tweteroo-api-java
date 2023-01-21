8096c4aa6af797e365c69da77d114d364275a6a6
Thales Alves <thalesalves758@gmail.com>
2023-01-21 12:43:58 -0300

    feat: create auth controller

---
+++ src/main/java/com/tweteroo/api/controller/AuthController.java
@@ -0,0 +1,39 @@
+package com.tweteroo.api.controller;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tweteroo.api.dto.SignUpDto;
+import com.tweteroo.api.exception.UserAlreadyExistsException;
+import com.tweteroo.api.model.TweterooUser;
+import com.tweteroo.api.service.AuthService;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/auth/sign-up")
+public class AuthController {
+  @Autowired
+  private AuthService authService;
+
+  @Autowired
+  private ModelMapper modelMapper;
+
+  @PostMapping()
+  public ResponseEntity<TweterooUser> signUp(@RequestBody @Valid SignUpDto body) {
+    TweterooUser user = modelMapper.map(body, TweterooUser.class);
+
+    try {
+      authService.signUp(user);
+      return new ResponseEntity<>(HttpStatus.CREATED);
+    } catch (UserAlreadyExistsException e) {
+      return new ResponseEntity<>(HttpStatus.CONFLICT);
+    }
+  }
+}
