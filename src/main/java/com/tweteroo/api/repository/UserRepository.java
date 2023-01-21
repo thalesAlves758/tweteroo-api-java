8f0fa32aec15361db0fb82d2ff9af5d5ba85bce0
Thales Alves <thalesalves758@gmail.com>
2023-01-21 13:06:13 -0300

    fix: change generics id type from Int to Long

--- src/main/java/com/tweteroo/api/repository/UserRepository.java
+++ src/main/java/com/tweteroo/api/repository/UserRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.tweteroo.api.model.TweterooUser;
 
-public interface UserRepository extends JpaRepository<TweterooUser, Integer> {
+public interface UserRepository extends JpaRepository<TweterooUser, Long> {
   public Optional<TweterooUser> findByUsername(String username);
 }
