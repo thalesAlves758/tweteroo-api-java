7aa88d180845f9c64a2188591968da294a9c4c5f
Thales Alves <thalesalves758@gmail.com>
2023-01-21 11:42:50 -0300

    feat: add id property

--- src/main/java/com/tweteroo/api/model/User.java
+++ src/main/java/com/tweteroo/api/model/User.java
@@ -1,7 +1,11 @@
 package com.tweteroo.api.model;
 
+import jakarta.annotation.Generated;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -9,6 +13,10 @@ import lombok.NoArgsConstructor;
 @Entity
 @NoArgsConstructor
 public class User {
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
+
   @Column(unique = true)
   private String username;
 
